a78c2518d7bf545840f60a34966bbf5b92c096cc
theonion <1554367213@qq.com>
2022-10-08 21:21:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/weather/gson/forecast.java
@@ -0,0 +1,26 @@
+package com.example.weather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class forecast {
+    public String data;
+
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class Temperature{
+        @SerializedName("tmp_max")
+        public String max;
+        @SerializedName("tmp_min")
+        public String  min;
+
+    }
+    public class More{
+
+        @SerializedName("txt_d")
+        public String info;
+    }
+}
